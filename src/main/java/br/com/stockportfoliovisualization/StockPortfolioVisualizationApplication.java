7539f17d9bc1b8c3a25b7a45e70a03c9df5c5ab3
RodrigoLimaM <digo100200@gmail.com>
2021-01-19 01:04:42 -0300

    initializing project

---
+++ src/main/java/br/com/stockportfoliovisualization/StockPortfolioVisualizationApplication.java
@@ -0,0 +1,13 @@
+package br.com.stockportfoliovisualization;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockPortfolioVisualizationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockPortfolioVisualizationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/stockportfoliovisualization/StockPortfolioVisualizationApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.stockportfoliovisualization;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockPortfolioVisualizationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
